2998a924fde726effdf1f2c3241d80290d4b0715
BAVANASI KUMAR REDDY <50985434+KUMAR-REDDY-BAVANASI@users.noreply.github.com>
2020-10-22 08:27:10 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hello World KUMAR  ");
+		get("/hello", (req, res) -> "Hello World");
 	}
 }
